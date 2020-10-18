51dd289309afbb4c64cb0e85c2150ec83dce2f3f
Keith Berry <keithwberry@gmail.com>
2020-10-17 22:23:36 -0400

    Create generate_wrangler.sh

---
+++ .github/scripts/generate_wrangler.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Generate wrangler.toml using secret variables
+
+cat > wrangler.toml <<- EOM
+name = "jkpenet"
+type = "webpack"
+account_id = "$CF_ACCOUNT_ID"
+workers_dev = false
+route = "jkpe.net/*"
+zone_id = "$CF_ZONE_ID"
+
+[site]
+bucket = "./public"
+entry-point = "jkpenet"
+EOM
